30662528f15340a04d844b3dded4e1058db56bce
Michael Allen <mallen@advisor360.com>
2023-01-24 09:47:16 +0000

    Refactor packet generation to allow for easier customisation, remove output packet streams

---
+++ src/main.py
@@ -0,0 +1,96 @@
+import random
+
+from pylibpcap import wpcap, rpcap
+
+
+FACTS = [b'There are over 1,000 varieties of cherries.',
+         b'The botanical name of the wild cherry tree is "prunus avium".',
+         b'Cherries are a good source of vitamin C.',
+         b'Cherries are rich in antioxidants and anti-inflammatory compounds.',
+         b'Cherries contain relatively high amounts of the metals potassium, copper, and manganese.',
+         b'Never eat cherry pits! They contain small amounts of amygdalin which your body converts to cyanide.']
+
+PADDING = [b'\xa0', b'\xa1', b'\xa2', b'\xa3', b'\xa4', b'\xa5', b'\xa6', b'\xa7', b'\xa8', b'\xa9']
+
+
+def read(filename, limit=16):
+    """ Read the contents of a pcap file and print details to the terminal.
+
+    Args:
+         filename (str): Path to a pcap file.
+         limit (int): The maximum number of packets to read from the pcap file.
+    """
+    i = 0
+    # Iterate through each packet read from the pcap file
+    for length, t, pkt in rpcap(filename):
+        print('Buf length:', length, 'Time:', t, 'Buf:', pkt)
+
+        # Break the loop if the limit of reads has been hit
+        i += 1
+        if i >= limit:
+            break
+
+
+def generate(packet_size, num_packets, num_consumers, do_read=True):
+    """ Generate a pcap file.
+
+    Args:
+         packet_size (int): The packet size in bytes.
+         num_packets (int): The number of packets in the stream.
+         num_consumers (int): The number of consumers intended for use.
+         do_read (bool): Whether to read the file after generation or not.
+    """
+    filename = f'../output/{packet_size:,}B_{num_packets:,}P_{num_consumers}C.pcap'
+
+    # Create/clear the contents of the pcap file
+    open(filename, 'w').close()
+
+    buffers = random.choices(PADDING[0:num_consumers], k=num_packets)
+    data = random.choices(FACTS, k=num_packets)
+
+    packets = []
+    for buf, fact in zip(buffers, data):
+        packet = buf * 16 + fact
+        packet += buf * (packet_size - len(packet))
+        packets.append(packet)
+
+    # Write the new contents
+    wpcap(packets, filename)
+
+    if do_read:
+        read(filename)
+
+
+if __name__ == '__main__':
+    #
+    # Generate packets
+    #
+
+    # Vary packet count
+    generate(128, 20_000, 2)
+    generate(128, 40_000, 2)
+    generate(128, 60_000, 2)
+    generate(128, 80_000, 2)
+    generate(128, 100_000, 2)
+    generate(128, 120_000, 2)
+    generate(128, 140_000, 2)
+    generate(128, 160_000, 2)
+    generate(128, 180_000, 2)
+    generate(128, 200_000, 2)
+
+    # Vary packet length
+    generate(128, 100_000, 2)
+    generate(256, 100_000, 2)
+    generate(512, 100_000, 2)
+    generate(1024, 100_000, 2)
+
+    # Vary consumer count
+    generate(128, 100_000, 2)
+    generate(128, 100_000, 3)
+    generate(128, 100_000, 4)
+    generate(128, 100_000, 5)
+    generate(128, 100_000, 6)
+    generate(128, 100_000, 7)
+    generate(128, 100_000, 8)
+    generate(128, 100_000, 9)
+    generate(128, 100_000, 10)
